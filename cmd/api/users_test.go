7fd76b19bb0050f076e2203729ac243fd8119795
AmiyoKm <games.amiyo@gmail.com>
2025-05-04 21:38:59 +0600

    test: enhance user retrieval tests with caching logic and mock store

--- cmd/api/users_test.go
+++ cmd/api/users_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"testing"
+
+	"github.com/AmiyoKm/go-backend/internal/store/cache"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetUser(t *testing.T) {
@@ -33,6 +36,11 @@ func TestGetUser(t *testing.T) {
 
 	t.Run("should allow authenticated requests", func(t *testing.T) {
 
+		mockCacheStore := app.CacheStorage.Users.(*cache.MockUserStore)
+
+		mockCacheStore.On("Get", int64(1)).Return(nil, nil).Twice()
+		mockCacheStore.On("Set", mock.Anything).Return(nil)
+
 		req, err := http.NewRequest(http.MethodGet, "/v1/users/1", nil)
 		if err != nil {
 			t.Fatal(err)
@@ -44,5 +52,54 @@ func TestGetUser(t *testing.T) {
 
 		checkResponseCode(t, http.StatusOK, rr.Code)
 
+		mockCacheStore.Calls = nil // Reset mock expectations
+
+	})
+	t.Run("should hit the cache first and if not exists it sets the user on the cache", func(t *testing.T) {
+		mockCacheStore := app.CacheStorage.Users.(*cache.MockUserStore)
+
+		mockCacheStore.On("Get", int64(1)).Return(nil, nil)
+		mockCacheStore.On("Get", int64(42)).Return(nil, nil)
+		mockCacheStore.On("Set", mock.Anything, mock.Anything).Return(nil)
+
+		req, err := http.NewRequest(http.MethodGet, "/v1/users/1", nil)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Header.Set("Authorization", "Bearer "+testToken)
+
+		rr := executeRequest(req, mux)
+
+		checkResponseCode(t, http.StatusOK, rr.Code)
+		mockCacheStore.Calls = nil // Reset mock expectations
+	})
+
+	t.Run("should NOT hit the cache if it is not enabled", func(t *testing.T) {
+		withRedis := Config{
+			redisCfg: redisConfig{
+				enabled: false,
+			},
+		}
+
+		app := newTestApplication(t, withRedis)
+		mux := app.mount()
+
+		mockCacheStore := app.CacheStorage.Users.(*cache.MockUserStore)
+
+		req, err := http.NewRequest(http.MethodGet, "/v1/users/1", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req.Header.Set("Authorization", "Bearer "+testToken)
+
+		rr := executeRequest(req, mux)
+
+		checkResponseCode(t, http.StatusOK, rr.Code)
+
+		mockCacheStore.AssertNotCalled(t, "Get")
+
+		mockCacheStore.Calls = nil // Reset mock expectations
 	})
 }
